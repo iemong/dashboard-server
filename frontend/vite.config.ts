b22a9e7d84a5bf9769d1596bcf6a524bcb4b76c9
iemong <kiyoshidainagon@gmail.com>
2021-05-20 01:36:09 +0900

    proxyの設定

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,15 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/meter': {
+        target: 'http://localhost:3000/meter',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/meter/, ''),
+      },
+    },
+  },
   build: {
     outDir: '../dist',
   },
