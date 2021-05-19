af6f1ca8d191abeb5eada28ac19a5b8acac150ea
iemong <kiyoshidainagon@gmail.com>
2021-05-20 00:05:49 +0900

    静的サイトのファイルを読み込めるように

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigModule } from '@nestjs/config';
 import { GrassModule } from './grass/grass.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env/default.env'],
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
     }),
     GrassModule,
   ],
