cc1357f1352ad1e1f2f6ee0711715b6f97364d17
iemong <kiyoshidainagon@gmail.com>
2021-05-21 11:39:50 +0900

    meterの保存を定期実行するように

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,12 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { MeterService } from '../meter/meter.service';
 
 @Injectable()
 export class TasksService {
   private readonly logger = new Logger(TasksService.name);
+  constructor(private meterService: MeterService) {}
 
   @Cron(CronExpression.EVERY_5_MINUTES)
-  handleCron() {
-    this.logger.debug('Called every 5 minutes');
+  async handleCron() {
+    await this.meterService.registerMeter();
+    this.logger.debug('registered current meter');
   }
 }
