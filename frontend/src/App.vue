0b6438dbe68162824d0eef7c1c05ff76a9d8411d
iemong <kiyoshidainagon@gmail.com>
2021-07-03 20:30:33 +0900

    100かけないと

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -73,7 +73,7 @@ handleFetch();
 
 const percentage = computed(() => {
   const result =  timeLength.value / wholeTimeLength.value
-  return isNaN(result) ? '計測できません' : `${Math.round(result)}%`
+  return isNaN(result) ? '計測できません' : `${Math.round(result * 100)}%`
 })
 
 const timerId = ref<number | null>(null);
